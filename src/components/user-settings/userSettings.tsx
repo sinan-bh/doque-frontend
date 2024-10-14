3796e1dae13cfc43950eddb13ad6e89b53e5685c
Sahad <zanusahad@gmail.com>
2024-10-14 11:40:20 +0530

    Feature/profile "update styles" (#3)
    * 86cwp7zvg created authentication pages
    
    * 86cwp7zvg setup navigation for auth
    
    * 86cwp7zvg adjusted length and size of the components
    
    * 86cwp7zvg add theme and reminder icon to navbar
    
    * merged
    
    * added user profile settings
    
    * merge latest changes
    
    * 86cwp7zvg:created user profile
    
    ---------
    
    Co-authored-by: Anas P <128923778+anaspxr@users.noreply.github.com>

--- src/components/user-settings/userSettings.tsx
+++ src/components/user-settings/userSettings.tsx
@@ -14,13 +14,11 @@ const ProfileSettings: React.FC = () => {
     };
 
     return (
-        <div className="min-h-screen bg-gray-100 p-6">
+        <div className="h-full bg-gray-100 p-6">
             <div className="mb-6">
                 <h2 className="text-3xl font-bold">My Settings</h2>
             </div>
-
-            {/* Profile Section */}
-            <div className="flex w-full rounded-lg max-w-6xl mx-auto overflow-hidden shadow-md"> 
+            <div className="flex w-full mr-10 rounded-lg max-w-6xl mx-auto overflow-hidden shadow-md">
                 <div className="flex-1 relative">
                     <div className="relative h-full bg-white p-6 flex items-center">
                         <div className="absolute top-0 left-0 w-full h-2/3 bg-cover bg-center" style={{ backgroundImage: 'url(https://images.unsplash.com/photo-1533134486753-c833f0ed4866?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8d2FsbHBhcGVyfGVufDB8fDB8fHww)' }}>
@@ -41,12 +39,9 @@ const ProfileSettings: React.FC = () => {
             </div>
 
             <div className="my-6"></div>
-
-            {/* Form Section */}
-            <div className="max-w-6xl mx-auto p-12 bg-white rounded-lg shadow-md relative"> 
+            <div className="max-w-6xl mr-10 mx-auto p-12  bg-white rounded-lg shadow-md relative">
                 <form onSubmit={handleSubmit} className="space-y-4">
                     <div className="flex space-x-8">
-                        {/* Left Side - Inputs */}
                         <div className="flex-1 space-y-4">
                             <div className="flex flex-col">
                                 <label className="text-sm font-medium text-gray-700">Full Name</label>
@@ -54,7 +49,7 @@ const ProfileSettings: React.FC = () => {
                                     type="text"
                                     value={name}
                                     onChange={(e) => setName(e.target.value)}
-                                    className="p-3 border border-gray-300 rounded-lg focus:ring focus:ring-blue-200"  
+                                    className="p-2 border border-gray-300 rounded-lg focus:ring focus:ring-blue-200"
                                     required
                                 />
                             </div>
@@ -64,7 +59,7 @@ const ProfileSettings: React.FC = () => {
                                     type="email"
                                     value={email}
                                     onChange={(e) => setEmail(e.target.value)}
-                                    className="p-3 border border-gray-300 rounded-lg focus:ring focus:ring-blue-200" 
+                                    className="p-2 border border-gray-300 rounded-lg focus:ring focus:ring-blue-200"
                                     required
                                 />
                             </div>
@@ -74,27 +69,29 @@ const ProfileSettings: React.FC = () => {
                                     type="password"
                                     value={password}
                                     onChange={(e) => setPassword(e.target.value)}
-                                    className="p-3 border border-gray-300 rounded-lg focus:ring focus:ring-blue-200" 
+                                    className="p-2 border border-gray-300 rounded-lg focus:ring focus:ring-blue-200"
                                 />
                             </div>
+                            <div className="mt-2">
+                                <a href="#" className="text-sm text-blue-500 flex justify-end hover:underline">Change Password</a>
+                            </div>
                         </div>
-
                         <div className="flex-1 flex flex-col justify-between space-y-4">
                             <div className="flex flex-col">
                                 <label className="text-sm font-medium text-gray-700">Description</label>
                                 <textarea
                                     value={description}
                                     onChange={(e) => setDescription(e.target.value)}
-                                    className="p-3 border border-gray-300 rounded-lg focus:ring focus:ring-blue-200"  
-                                    rows={6}  
+                                    className="p-3 border border-gray-300 rounded-lg focus:ring focus:ring-blue-200"
+                                    rows={6}
                                 />
                             </div>
                             <div className="mt-auto">
                                 <button
                                     type="submit"
-                                    className="px-8 py-3 bg-blue-500 text-white rounded-lg shadow-sm hover:bg-blue-600 transition duration-200"  
+                                    className="px-6 py-2 mr-2 bg-[#F6F8FA] float-end text-black border border-gray-500 rounded-lg shadow-lg hover:bg-gray-100 transition duration-200"
                                 >
-                                    Save Changes
+                                    Save 
                                 </button>
                             </div>
                         </div>
